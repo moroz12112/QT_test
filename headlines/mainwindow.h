d17d1b57054df8cdab910804516e8ea1b188d913
I F Gushchin @ Dell G3 Ubuntu 18.04 x64 <konara@yandex.ru>
2022-05-04 10:22:32 +0400

    example of several slots and signals
    пример автоматически связываемого слота (с пред-определённым сигналом)
    пример связывания штатного сигнала со своим слотом
    пример связывания своего сигнала со своим слотом

--- headlines/mainwindow.h
+++ headlines/mainwindow.h
@@ -15,7 +15,18 @@ public:
     explicit MainWindow(QWidget *parent = 0 );
     ~MainWindow();
 
+signals:
+    void rotate(int deg_cw);
+
+public slots:
+    void receiveRotate(int def);
+    void on_clac_clicked();
+    void manual_connection_manu();
+    void rotateReceiver(int);
+
+
 private:
+    int cc;
 
     Ui::Dialog *ui;
 
--- sources/mainwindow.cpp
+++ sources/mainwindow.cpp
@@ -3,14 +3,43 @@
 # include "../forms/ui_untitled.h"
 
 
+#include <iostream>
+#include <unistd.h>
+
+
 MainWindow::MainWindow(QWidget *parent):
-    QDialog (parent),
+    QDialog (parent), cc(0),
     ui(new Ui::Dialog)
 {
     ui->setupUi(this);
+
+    connect(ui->manu, SIGNAL(clicked()), this, SLOT(manual_connection_manu()));
+    connect(this, SIGNAL(rotate(int)), this, SLOT(receiveRotate(int)));
+    connect(this, SIGNAL(rotate(int)), this, SLOT(rotateReceiver(int)));
 }
 
 MainWindow::~MainWindow()
 {
     delete ui;
+}
+
+void MainWindow::receiveRotate(int def)
+{
+    std::cout<<"receiveRotate with value "<<def<<std::endl;
+    ui->schr->setText(QString("%1").arg(def) + "        " + ui->schr->text());
+}
+
+void MainWindow::on_clac_clicked()
+{
+    emit rotate(cc++);
+}
+
+void MainWindow::manual_connection_manu()
+{
+    emit rotate(--cc);
+}
+
+void MainWindow::rotateReceiver(int v)
+{
+    ui->schr->setText(QString("%1").arg(v));
 }
