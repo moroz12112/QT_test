f0e5f88964135cc2a586b9cf333edccf50bb0167
I F Gushchin @ Dell G3 Ubuntu 18.04 x64 <konara@yandex.ru>
2022-05-04 10:56:34 +0400

    example of PkgConfig settings for CMake
    в CMake импортируется модуль PkgConfig
    с его помощью ищется orocos-kdl и добавляются необходимые пути
    для поиска заголовочных файлов, библиотек.
    так же добавлен флаг линкера для линковки с библиотекой KDL

--- headlines/mainwindow.h
+++ headlines/mainwindow.h
@@ -32,4 +32,26 @@ private:
 
 };
 
+#define JOINTS_COUNT 6
+#include <kdl/chainiksolverpos_nr_jl.hpp>
+#include <kdl/chain.hpp>
+#include <kdl/frames.hpp>
+#include <kdl/frames_io.hpp>
+#include <kdl/chainfksolver.hpp>
+#include <kdl/chainfksolverpos_recursive.hpp>
+#include <kdl/chainfksolverpos_recursive.hpp>
+#include <kdl/chainiksolvervel_pinv.hpp>
+#include <kdl/chainiksolvervel_wdls.hpp>
+#include <kdl/chainiksolverpos_nr.hpp>
+
+class KDL_CHAIN {
+    double j[JOINTS_COUNT];
+    KDL::Chain ourChain;
+
+  public:
+    KDL_CHAIN(double j1=0., double j2=0., double j3=0., double j4=0, double j5=0, double j6=0);
+    bool evaluate();
+    double getX() const;
+};
+
 #endif
